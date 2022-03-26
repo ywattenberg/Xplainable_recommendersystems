469d6a310b94fdffb2c94826b73a72f55c180227
wattenberg <wattenberg@mailbox.org>
2022-03-26 15:03:12 +0100

    prepare script

---
+++ prepare_dataset.py
@@ -0,0 +1,38 @@
+from webbrowser import get
+import pandas as pd
+import json
+from AmazonCSJDataset import AmazonCSJDataset
+    
+def main():
+    df = getDF('data/Clothing_Shoes_and_Jewelry_5.json')
+    df = df[['overall', 'reviewerID', 'asin', 'unixReviewTime']]
+    df, num_users, num_products, user_ids, product_ids = encode_df(df)
+    df['rank_latest'] = df.groupby(['reviewerID'])['unixReviewTime'].rank(method='first', ascending=False)
+    df.to_csv('data\\compact_CSJ.csv', index=False)
+
+def parse(path):
+    with open(path, 'rb') as file:
+        for l in file:
+            yield json.loads(l)
+
+def getDF(path):
+    i = 0
+    df = {}
+    for d in parse(path):
+        df[i] = d
+        i += 1
+    return pd.DataFrame.from_dict(df, orient='index')
+
+def encode_cols(self, column):
+        keys = column.unique()
+        key_to_id = {key:idx for idx,key in enumerate(keys)}
+        return key_to_id, np.array(key_to_id[x] for x in column), len(keys)
+
+def encode_df(self, df):
+    product_ids, df['asin'], num_products = self.encode_cols(df['asin'])
+    user_ids, df['reviewerID'], num_users = self.encode_cols(df['reviewerID'])
+    return df, num_users, num_products, user_ids, product_ids
+
+if __name__ == '__main__':
+    main()
+    
