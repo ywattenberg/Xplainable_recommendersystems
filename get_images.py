7339d09499c154372cc14af7d064f4c6868dc630
wattenberg <wattenberg@mailbox.org>
2022-04-03 17:12:56 +0200

    how many needed

--- get_images.py
+++ get_images.py
@@ -20,28 +20,28 @@ def main(path):
             got.add(asin)
 
     needed = needed - got
-    
+
     print(len(needed))
 
-    json_list = parse(path)
-    for element in json_list:
-        asin = element['asin']
-        if 'imageURL' in element.keys():
-            urls = element['imageURL']
-            ending = urls[0].split('.')[-1] 
-            r = requests.get(urls[0], stream=True)
-            if r.status_code == 200:
-                r.raw.decode_content = True
-                with open(f'./data/images/{asin}.{ending}', 'wb') as f:
-                    shutil.copyfileobj(r.raw, f)
-                print(f'got image for {asin}')
-            else:
-                with open('failed.txt', 'a') as file:
-                    file.write(f'{asin}; {urls}\n')
-        else:
-            # with open('data/no_image.txt', 'a') as file:
-            #     file.write(f'{asin}\n')
-            pass
+    # json_list = parse(path)
+    # for element in json_list:
+    #     asin = element['asin']
+    #     if 'imageURL' in element.keys():
+    #         urls = element['imageURL']
+    #         ending = urls[0].split('.')[-1] 
+    #         r = requests.get(urls[0], stream=True)
+    #         if r.status_code == 200:
+    #             r.raw.decode_content = True
+    #             with open(f'./data/images/{asin}.{ending}', 'wb') as f:
+    #                 shutil.copyfileobj(r.raw, f)
+    #             print(f'got image for {asin}')
+    #         else:
+    #             with open('failed.txt', 'a') as file:
+    #                 file.write(f'{asin}; {urls}\n')
+    #     else:
+    #         # with open('data/no_image.txt', 'a') as file:
+    #         #     file.write(f'{asin}\n')
+    #         pass
 
 def getLines(path):
     lines = []
