2632bf0c835d1fe32b5272c1a1ef4b183f6c82eb
Wattenberg <wattenberg@mailbox.org>
2022-04-04 15:09:07 +0200

    concurent download with logs

--- get_images.py
+++ get_images.py
@@ -1,59 +1,65 @@
-from asyncore import write
-from operator import ne
 import requests
 import shutil
 from dataset.amazon_dataset_utils import parse
-import os
-import pandas as pd
-import threading
+from concurrent.futures import ThreadPoolExecutor
+import logging
 
-def main(thread_num):
+def main():
     path = 'data/meta_Clothing_Shoes_and_Jewelry.json'
     needed = set([])
     with open('needed.txt', 'r') as file:
         for line in file.readlines():
             needed.add(line.replace('\n', ''))
 
+    pool = ThreadPoolExecutor(16)
+    futures = []
+    counter = 0
+    lines = []
+    for line in parse(path):
+        counter += 1
+        lines.append(line)
+        if counter == 100:
+            counter = 0
+            futures.append(pool.submit(function=download, args=(lines, needed)))
+            lines = []
+    futures.append(pool.submit(function=download, args=(lines, needed)))
+
+    for task in futures:
+        task.join()
+
+def download(json_list, needed):
     # got = set([])
     # with open('downloaded.txt', 'r') as file:
     #     for line in file.readlines():
     #         asin = line.split('.')[0]
     #         got.add(asin)
-
-    needed = needed
-
-    json_list = parse(path)
-    json_list = json_list[thread_num*167816:(thread_num+1)*167816]
-    with open(f'no_image{thread_num}.txt', 'a') as noimage_file:
-        with open(f'failed{thread_num}.txt', 'a') as failed_file:
-            with open(f'downloaded{thread_num}.txt', 'a') as succ_file:
-                for element in json_list:
-                    asin = element['asin']
-                    if asin in needed:
-                        if 'imageURL' in element.keys():
-                            urls = element['imageURL']
-                            ending = urls[0].split('.')[-1] 
-                            try:
-                                r = requests.get(urls[0], stream=True)
-                                if r.status_code == 200:
-                                    r.raw.decode_content = True
-                                    with open(f'./data/images/{asin}.{ending}', 'wb') as f:
-                                        shutil.copyfileobj(r.raw, f)
-                                    succ_file.write(f'{asin}\n')
-                                else:
-                                    failed_file.write(f'{asin}; {urls}\n')
-                            except:
-                                failed_file.write(f'{asin}; {urls}\n')
-                        else:
-                            noimage_file.write(f'{asin}\n')
+    for element in json_list:
+        asin = element['asin']
+        if asin in needed:
+            if 'imageURL' in element.keys():
+                urls = element['imageURL']
+                ending = urls[0].split('.')[-1] 
+                try:
+                    r = requests.get(urls[0], stream=True)
+                    if r.status_code == 200:
+                        r.raw.decode_content = True
+                        with open(f'./data/images/{asin}.{ending}', 'wb') as f:
+                            shutil.copyfileobj(r.raw, f)
+                        logger.info(f'GOT {asin}\n')
+                    else:
+                        logger.info(f'FAILED {asin}; {urls}\n')
+                except:
+                    logger.info(f'FAILED {asin}; {urls}\n')
+            else:
+                logger.info(f'NO {asin}\n')
 
 
 if __name__ == '__main__':
-    threads = []
+    logpath = "./get_images.log"
+    logger = logging.getLogger('log')
+    logger.setLevel(logging.INFO)
+    ch = logging.FileHandler(logpath)
+    ch.setFormatter(logging.Formatter('%(message)s'))
+    logger.addHandler(ch)
 
-    for idx in range(16):
-        threads.append(threading.Thread(target=main, args=(idx,)))
-        threads[-1].start()
-
-    for thread in threads:
-        thread.join()
+    main()
