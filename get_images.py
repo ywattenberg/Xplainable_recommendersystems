bc0a41112d2d94e301c8d864025a5a4427c748b3
Wattenberg <wattenberg@mailbox.org>
2022-04-04 11:09:45 +0200

    multi thread

--- get_images.py
+++ get_images.py
@@ -5,53 +5,55 @@ import shutil
 from dataset.amazon_dataset_utils import parse
 import os
 import pandas as pd
+import threading
 
-def main(path):
-    needed = []
+def main(thread_num):
+    path = 'data/meta_Clothing_Shoes_and_Jewelry.json'
+    needed = set([])
     with open('needed.txt', 'r') as file:
         for line in file.readlines():
-            needed.append(line.replace('\n', ''))
+            needed.add(line.replace('\n', ''))
 
-    needed = set(needed)
-    got = set([])
-    with open('downloaded.txt', 'r') as file:
-        for line in file.readlines():
-            asin = line.split('.')[0]
-            got.add(asin)
+    # got = set([])
+    # with open('downloaded.txt', 'r') as file:
+    #     for line in file.readlines():
+    #         asin = line.split('.')[0]
+    #         got.add(asin)
 
-    needed = needed - got
+    needed = needed
 
     json_list = parse(path)
-    for element in json_list:
-        asin = element['asin']
-        if asin in needed:
-            if 'imageURL' in element.keys():
-                urls = element['imageURL']
-                ending = urls[0].split('.')[-1] 
-                try:
-                    r = requests.get(urls[0], stream=True)
-                    if r.status_code == 200:
-                        r.raw.decode_content = True
-                        with open(f'./data/images/{asin}.{ending}', 'wb') as f:
-                            shutil.copyfileobj(r.raw, f)
-                        print(f'got image for {asin}')
-                    else:
-                        with open('failed.txt', 'a') as file:
-                            file.write(f'{asin}; {urls}\n')
-                except:
-                    with open('failed.txt', 'a') as file:
-                            file.write(f'{asin}; {urls}\n')
-            else:
-                with open('data/no_image.txt', 'a') as file:
-                    file.write(f'{asin}\n')
-            pass
+    json_list = json_list[thread_num*167816:(thread_num+1)*167816]
+    with open(f'no_image{thread_num}.txt', 'a') as noimage_file:
+        with open(f'failed{thread_num}.txt', 'a') as failed_file:
+            with open(f'downloaded{thread_num}.txt', 'a') as succ_file:
+                for element in json_list:
+                    asin = element['asin']
+                    if asin in needed:
+                        if 'imageURL' in element.keys():
+                            urls = element['imageURL']
+                            ending = urls[0].split('.')[-1] 
+                            try:
+                                r = requests.get(urls[0], stream=True)
+                                if r.status_code == 200:
+                                    r.raw.decode_content = True
+                                    with open(f'./data/images/{asin}.{ending}', 'wb') as f:
+                                        shutil.copyfileobj(r.raw, f)
+                                    succ_file.write(f'{asin}\n')
+                                else:
+                                    failed_file.write(f'{asin}; {urls}\n')
+                            except:
+                                failed_file.write(f'{asin}; {urls}\n')
+                        else:
+                            noimage_file.write(f'{asin}\n')
 
-def getLines(path):
-    lines = []
-    with open(path, 'r') as file:
-        for l in file:
-            lines.append(l.split('\n')[0])
-    return lines
 
 if __name__ == '__main__':
-    main('data/meta_Clothing_Shoes_and_Jewelry.json')
+    threads = []
+
+    for idx in range(16):
+        threads.append(threading.Thread(target=main, args=(idx,)))
+        threads[-1].start()
+
+    for thread in threads:
+        thread.join()
