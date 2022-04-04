29a44fe021ac12d69c3f72dc7baffba854c46a33
Wattenberg <wattenberg@mailbox.org>
2022-04-04 20:08:16 +0200

    start impl of image extraction

--- model/SimpleMatrixFactorization.py
+++ model/SimpleMatrixFactorization.py
@@ -1,9 +1,15 @@
+from pyexpat import model
 import torch
 import numpy as np
+from torchvision import models
+
 class ModelMatrixFactorization(torch.nn.Module):
 
     def __init__(self, num_users, num_items, n_factors=20):
         super().__init__()
+
+        self.vvg16 = models.vgg16(pretrained=True)
+
         self.user_factors = torch.nn.Embedding(num_users, n_factors, sparse=True)
         self.item_factors = torch.nn.Embedding(num_items, n_factors, sparse=True)
         self.user_biases = torch.nn.Embedding(num_users, 1)
