09350152c89f6117b73418e92570a1bd61995bb6
Wattenberg <wattenberg@mailbox.org>
2022-04-15 18:35:27 +0200

    move baseline to device to avoid error at runtime

--- integrated_gradients.py
+++ integrated_gradients.py
@@ -23,8 +23,8 @@ def main():
     
     test_data = df[df['rank_latest'] == 1]
 
-    white_base_img = torch.ones([1,3,50,50], dtype=torch.float32, requires_grad=True)
-    black_base_img = torch.zeros([1,3,50,50], dtype=torch.float32, requires_grad=True)
+    white_base_img = torch.ones([1,3,50,50], dtype=torch.float32, requires_grad=True).to(device)
+    black_base_img = torch.zeros([1,3,50,50], dtype=torch.float32, requires_grad=True).to(device)
 
     ig = IntegratedGradients(model)
 
@@ -38,7 +38,9 @@ def main():
 
     img_attr_b, delta_b = ig.attribute((img_input), baselines=(black_base_img), additional_forward_args=(user_input, product_input), n_steps=200, method='gausslegendre',return_convergence_delta=True)
 
-    img_attr_w, delta_w = ig.attribute((img_input), baselines=(white_base_img), additional_forward_args=(user_input, product_input), n_steps=200, method='gausslegendre',return_convergence_delta=True) 
+    img_attr_w, delta_w = ig.attribute((img_input), baselines=(white_base_img), additional_forward_args=(user_input, product_input), n_steps=200, method='gausslegendre',return_convergence_delta=True)
+
+
 
 def plot_attributions(image, baseline, attribution_mask):
     # convert attribution array into displayable image
