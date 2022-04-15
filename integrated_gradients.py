172a61475acebe6360577152fae6ac55b0f038a2
Wattenberg <wattenberg@mailbox.org>
2022-04-15 18:56:00 +0200

    tensors have to be on cpu

--- integrated_gradients.py
+++ integrated_gradients.py
@@ -46,9 +46,9 @@ def main():
 
 
 def plot_attributions(image, baseline, attribution_mask, alpha=0.4):
-    image = image.squeeze()
-    baseline = baseline.squeeze()
-    attribution_mask = attribution_mask.squeeze()
+    image = image.squeeze().cpu()
+    baseline = baseline.squeeze().cpu()
+    attribution_mask = attribution_mask.squeeze().cpu()
     
     fig = plt.figure(figsize=(10,10))
 
