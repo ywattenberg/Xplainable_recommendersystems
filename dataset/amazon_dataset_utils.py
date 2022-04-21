bbca33ddd8d708a06de6e4664d12e6b75be45d7e
Wattenberg <wattenberg@mailbox.org>
2022-04-21 21:59:40 +0200

    user gpu 1

--- dataset/amazon_dataset_utils.py
+++ dataset/amazon_dataset_utils.py
@@ -26,27 +26,27 @@ def encode_cols(column):
 def encode_df(df):
     product_ids, df['productID'], num_products = encode_cols(df['asin'])
     user_ids, df['userID'], num_users = encode_cols(df['reviewerID'])
-    return df, num_users, num_products, user_ids, product_ids
+    return df
 
 def prepare_dataset(path):
     df = getDF(path)
     df = df[['overall', 'reviewerID', 'asin', 'unixReviewTime']]
-    df, num_users, num_products, user_ids, product_ids = encode_df(df)
+    df = encode_df(df)
     df['rank_latest'] = df.groupby(['reviewerID'])['unixReviewTime'].rank(method='first', ascending=False)
     df.to_csv('data/compact_CSJ.csv', index=False)
     return df
 
 def label_transform(z):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
     return torch.tensor(z, dtype=torch.float32).to(device)
 
 def transform(z):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
     tmp = torch.tensor(z).to(device)
     #tmp.requires_grad_()
     return tmp
 
 def image_transform(img):
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
     transform = transforms.Compose([transforms.Resize(500), transforms.CenterCrop(500), transforms.ToTensor()])
     return transform(img).to(device)
