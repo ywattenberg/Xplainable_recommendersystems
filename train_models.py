8b6be72d66b3d822a8e6ea22747827e2fd7b2bca
Wattenberg <wattenberg@mailbox.org>
2022-06-07 18:28:23 +0200

    refactor train/test infastruct

---
+++ train_models.py
@@ -0,0 +1,47 @@
+import imp
+from pickletools import optimize
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from dataset.amazon_csj_dataset import AmazonCSJDatasetWithIMGHD, AmazonCSJDatasetWithIMG
+from model.MatrixFactorizationWithImages import get_MF_with_images_vgg16, get_MF_with_images_EfficentNetB4, get_MF_with_images_Mixerl16
+from dataset.amazon_dataset_utils import *
+from trainer import Trainer
+
+
+def get_trainer_imageHD(model_fn):
+    df = pd.read_csv('/mnt/ds3lab-scratch/ywattenberg/data/compact_CSJ_imgHD.csv')
+    train_data = df[df['rank_latest'] != 1]
+    test_data = df[df['rank_latest'] == 1]
+    num_users = df['reviewerID'].nunique()
+    num_items = df['asin'].nunique()
+
+    train_data = AmazonCSJDatasetWithIMG(path=None, df=train_data)
+    test_data = AmazonCSJDatasetWithIMG(path=None, df=test_data)
+
+    model = model_fn(num_items=num_items, num_users=num_users)
+    
+    loss_fn = torch.nn.MSELoss()
+    optimize = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-8)
+    
+    return Trainer(model, loss_fn, optimize, train_data, test_data, batch_size=80, epochs=4)
+
+
+def get_trainer_vgg16_HD():
+   return get_trainer_imageHD(get_MF_with_images_vgg16)
+
+def get_trainer_mixer_HD():
+    return get_trainer_imageHD(get_MF_with_images_Mixerl16)
+
+def get_trainer_efficent_HD():
+    return get_trainer_imageHD(get_MF_with_images_EfficentNetB4)
+
+if __name__ == '__main__':
+    #trainer = get_trainer_vgg16_HD()
+    #trainer = get_trainer_efficent_HD()
+    trainer = get_trainer_mixer_HD()
+    trainer.train_test()
+    
---
+++ trainer.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import torch
+import warnings
+from datetime import datetime
+
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+
+
+
+class Trainer():
+    def __init__(self, model, train_data, test_data, loss_fn, split_test:float=None, optimizer=None, device=None, batch_size=32, num_epochs=10, shuffle=True, name=None):
+        if model == None or train_data == None:
+            raise Exception("Model and train_data must be specified")
+
+        if test_data == None:
+            if split_test == None:
+                raise Exception("Must specify either test_data or split_train")
+            elif split_test != None:
+                #warnings.warn("Test data is not specified, will split train data into train and validation")
+                train_len = int(len(test_data*(1-split_test)))
+                test_len = len(test_data) - train_len
+                train_data, test_data = random_split(train_data, (train_len, test_len))
+
+        if optimizer == None:
+            self.optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-8)
+        else:    
+            self.optimizer = optimizer
+
+        if device == None:
+            self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        else:
+            self.device = device
+
+        if name == None:
+            self.name = datetime.now().strftime("%Y-%m-%d_%H")
+        else:
+            self.name = name
+
+        self.batch_size = batch_size
+        self.num_epochs = num_epochs
+        self.model = model.to(self.device)
+        self.loss_fn = loss_fn
+
+        self.train_dataloader = DataLoader(train_data, batch_size=self.batch_size, shuffle=shuffle)
+        self.test_dataloader = DataLoader(test_data, batch_size=self.batch_size, shuffle=shuffle)
+
+    def train_loop(self):
+        size = len(self.train_dataloader.dataset)
+        for batch, (*input, y) in enumerate(self.train_dataloader):
+            self.optimizer.zero_grad()
+            pred = self.model(*input)
+            loss = self.loss_fn(pred, y)
+            loss.backward()
+            self.optimizer.step()
+
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * len(input[0])
+                print(f'loss: {loss:>7f}  [{current:>5d}/{size:>5d}]')
+            
+            if batch % 1000 == 0:
+                torch.save(self.model, 'tmp_entire_model_imp.pth')
+
+    def test_loop(self):
+        size = len(self.test_dataloader.dataset)
+        num_batches = len(self.test_dataloader)
+        test_loss, correct = 0, 0
+
+        with torch.no_grad():
+            for batch, (*input, y) in enumerate(self.test_dataloader):
+                pred = self.model(*input)
+                loss = self.loss_fn(pred, y)
+                test_loss += loss.item()
+                correct += (pred - y).abs().type(torch.float).sum().item()
+
+                if batch % 100 == 0:
+                    print(f'Current testloss: {test_loss / batch:>8f}')
+
+        test_loss /= num_batches
+        correct /= size
+        print(f'Test Error \n Accuracy: {(100 * correct):>1f}%, Avg loss: {test_loss:>8f}')
+
+
+    def train_test(self):
+        try:
+            for t in range(self.num_epochs):
+                    print(f"Epoch {t + 1}\n-------------------------------")
+                    self.train_loop()
+                    self.test_loop()
+            print("Done!")
+            torch.save(self.model.state_dict(), f'model_weights_{self.name}.pth')
+            torch.save(self.model, f'entire_model_{self.name}.pth')
+        except KeyboardInterrupt:
+            print('Abort...')
+            safe = input('Safe model [y]es/[n]o: ')
+            if safe == 'y' or safe == 'Y':
+                torch.save(self.model.state_dict(), f'model_weights_{self.name}.pth')
+                torch.save(self.model, f'entire_model_{self.name}.pth')
+            else: 
+                print('Not saving...')
