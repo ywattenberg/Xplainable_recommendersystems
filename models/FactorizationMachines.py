b6656cc1f62042dd0bcca2d937c1d854c15f0c5c
Wattenberg <wattenberg@mailbox.org>
2022-06-23 15:32:30 +0200

    add code for FM

--- models/FactorizationMachines.py
+++ models/FactorizationMachines.py
@@ -5,19 +5,85 @@ import numpy as np
 
 class FactorizationMachineModel(torch.nn.Module):
 
-    def __init__(self, field_dims, embedding_dim):
+    def __init__(self, field_dims, embed_dim, mlp_dims, dropout):
         super().__init__()
-
-        self.V = nn.Parameter(torch.randn(sum(field_dims), embedding_dim),requires_grad=True)
-        self.lin = nn.Linear(sum(field_dims), 1)
-        
+        self.linear = FeaturesLinear(field_dims)
+        self.fm = FactorizationMachine(reduce_sum=True)
+        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
+        self.embed_output_dim = len(field_dims) * embed_dim
+        self.mlp = MultiLayerPerceptron(self.embed_output_dim, mlp_dims, dropout)
 
     def forward(self, x):
-        out_1 = torch.matmul(x, self.V).pow(2).sum(1, keepdim=True) #S_1^2
-        out_2 = torch.matmul(x.pow(2), self.V.pow(2)).sum(1, keepdim=True) # S_2
-        
-        out_inter = 0.5*(out_1 - out_2)
-        out_lin = self.lin(x)
-        out = out_inter + out_lin
-        
-        return out
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        embed_x = self.embedding(x)
+        x = self.linear(x) + self.fm(embed_x) + self.mlp(embed_x.view(-1, self.embed_output_dim))
+        return torch.relu(x.squeeze(1))
+
+class MultiLayerPerceptron(torch.nn.Module):
+
+    def __init__(self, input_dim, embed_dims, dropout, output_layer=True):
+        super().__init__()
+        layers = list()
+        for embed_dim in embed_dims:
+            layers.append(torch.nn.Linear(input_dim, embed_dim))
+            layers.append(torch.nn.BatchNorm1d(embed_dim))
+            layers.append(torch.nn.ReLU())
+            layers.append(torch.nn.Dropout(p=dropout))
+            input_dim = embed_dim
+        if output_layer:
+            layers.append(torch.nn.Linear(input_dim, 1))
+        self.mlp = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        """
+        :param x: Float tensor of size ``(batch_size, embed_dim)``
+        """
+        return self.mlp(x)
+
+class FeaturesLinear(torch.nn.Module):
+
+    def __init__(self, field_dims, output_dim=1):
+        super().__init__()
+        self.fc = torch.nn.Embedding(sum(field_dims), output_dim)
+        self.bias = torch.nn.Parameter(torch.zeros((output_dim,)))
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return torch.sum(self.fc(x), dim=1) + self.bias
+
+class FeaturesEmbedding(torch.nn.Module):
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(sum(field_dims), embed_dim)
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        torch.nn.init.xavier_uniform_(self.embedding.weight.data)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return self.embedding(x)
+class FactorizationMachine(torch.nn.Module):
+
+    def __init__(self, reduce_sum=True):
+        super().__init__()
+        self.reduce_sum = reduce_sum
+
+    def forward(self, x):
+        """
+        :param x: Float tensor of size ``(batch_size, num_fields, embed_dim)``
+        """
+        square_of_sum = torch.sum(x, dim=1) ** 2
+        sum_of_square = torch.sum(x ** 2, dim=1)
+        ix = square_of_sum - sum_of_square
+        if self.reduce_sum:
+            ix = torch.sum(ix, dim=1, keepdim=True)
+        return 0.5 * ix
