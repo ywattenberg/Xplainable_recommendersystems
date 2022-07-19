ec2cf14c3189beffad23f6a1dbc86d3511741316
Wattenberg <wattenberg@mailbox.org>
2022-07-19 12:33:23 +0200

    add code to load review examples

---
+++ gen_previous.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import os
+
+from dataset.amazon_dataset_utils import *
+
+def main():
+    test_df = pd.read_csv('/mnt/ds3lab-scratch/ywattenberg/data/compact_CSJ_imgHD_subset_test.csv')
+    train_df = pd.read_csv('/mnt/ds3lab-scratch/ywattenberg/data/compact_CSJ_imgHD_subset_train.csv')
+    test_users = set(test_df.reviewerID.unique())
+    train_products = set(train_df.asin.unique())
+    test_user_idx = dict(zip(test_users, np.zeros(len(test_users), dtype=int)))
+
+    reviews = pd.DataFrame(columns=['reviewerID', 'asin', 'overall', 'reviewText'])
+
+    i = 0
+    for line in parse('/mnt/ds3lab-scratch/ywattenberg/data/Clothing_Shoes_and_Jewelry_5.json'):
+        if line['asin'] in train_products and line['reviewerID'] in test_users and test_user_idx[line['reviewerID']] < 5:
+            i += 1
+            reviews.loc[i] = [line['reviewerID'], line['asin'], line['overall'], line['reviewText']]
+            test_user_idx[line['reviewerID']] += 1
+        if i > 1000:
+            break
+    reviews.to_csv('/mnt/ds3lab-scratch/ywattenberg/data/reviews_examples.csv', index=False)
+
+            
+
