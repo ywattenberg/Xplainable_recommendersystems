9839fb28644dd0c4d2e2990339536b4f68579e2f
Yannick Wattenberg <ywattenberg@spaceml1.ethz.ch>
2022-08-27 17:32:51 +0200

    change to work

--- scripts/test_IG.py
+++ scripts/test_IG.py
@@ -1,21 +1,26 @@
 from torchvision import models, transforms
 from PIL import Image
 from captum.attr import IntegratedGradients
+import torch
+import numpy as np
 
 def main():
-    model = models.efficientnet_b7(pretrained=True)
-    model.eval()
+    device = 'cuda'
+    model = models.efficientnet_b3(pretrained=True)
+    model.to(device).eval()
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
     image = Image.open('cat.jpg')
     image = transform(image)
-    image = image.unsqueeze(0)
-    attr = IntegratedGradients(model).attribute(image)
+    image = image.to(device)
+    baseline = torch.zeros(image.shape).to(device).unsqueeze(dim=0)
+    print(baseline.shape)
+    attr = IntegratedGradients(model).attribute(image, baselines=baseline, internal_batch_size=1)
 
     #print(output)
     #ans = output.squeeze().detach().numpy().tolist()
     #print(ans.index(max(ans)))
     to_img = transforms.ToPILImage()
-    to_img(attr).save('C:/Users/User/Downloads/car_attr.jpg')
+    to_img(attr.to('cpu')).save('C:/Users/User/Downloads/car_attr.jpg')
 
 
 
