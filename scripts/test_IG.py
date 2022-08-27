0cd33caad4e293a0b07ac8e324db4fc54daa6fef
Wattenberg <wattenberg@mailbox.org>
2022-08-27 17:33:10 +0200

    user new model

--- scripts/test_IG.py
+++ scripts/test_IG.py
@@ -1,9 +1,16 @@
 from torchvision import models, transforms
 from PIL import Image
 from captum.attr import IntegratedGradients
+import torch
+class s_model(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.main = models.efficientnet_b7(pretrained=True)
+    def forward(self, x):
+        return torch.nn.Softmax(self.main(x))
 
 def main():
-    model = models.efficientnet_b7(pretrained=True)
+    model = s_model()
     model.eval()
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
     image = Image.open('cat.jpg')
