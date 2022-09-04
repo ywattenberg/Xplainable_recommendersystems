9af314b5813046d5a7881250a445994525eadef0
Yannick Wattenberg <ywattenberg@spaceml1.ethz.ch>
2022-09-04 11:22:18 +0200

    add simple model

--- train_models.py
+++ train_models.py
@@ -5,8 +5,9 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms as T
 
-from dataset.amazon_csj_dataset import AmazonCSJDatasetWithIMGHD, AmazonCSJDatasetWithIMG
+from dataset.amazon_csj_dataset import AmazonCSJDatasetWithIMGHD, AmazonCSJDatasetWithIMG, AmazonCSJDataset
 from models.MatrixFactorizationWithImages import get_MF_with_images_Mixer12_split, get_MF_with_images_Efficent_split, get_MF_with_images_vgg16,  get_MF_with_images_EfficentNetB4, get_MF_with_images_Mixerl16, get_MF_only_images_Mixer12
+from models.SimpleMatrixFactorization import ModelMatrixFactorization
 from dataset.amazon_dataset_utils import *
 from trainer import Trainer
 
@@ -30,7 +31,28 @@ def get_trainer_imageHD(model_fn, timm_model=False, image_transform=None, path_t
 
     train_data = AmazonCSJDatasetWithIMGHD(path=None, df=train_data, prev_image_transform=image_transform, img_path=img_path)
     test_data = AmazonCSJDatasetWithIMGHD(path=None, df=test_data, prev_image_transform=image_transform, img_path=img_path)
+    loss_fn = torch.nn.MSELoss()
+    optimizer = optimizer_fn(model.parameters(), lr=lr, weight_decay=weight_decay)
+    
+    return Trainer(model, train_data, test_data, loss_fn, optimizer, batch_size=batch_size, epochs=epochs)
 
+def get_trainer_simple(path_to_csv='/mnt/ds3lab-scratch/ywattenberg/data/compact_CSJ_imgHD_subset_train.csv', img_path=None , epochs=4, batch_size=512, lr=0.01, weight_decay=0.0, optimizer_fn=torch.optim.SGD, loss_fn=torch.nn.MSELoss):
+    
+    df = pd.read_csv(path_to_csv)
+    train_data = df[df['rank_latest'] != 1]
+    test_data = df[df['rank_latest'] == 1]
+    #train_data = pd.read_csv('/mnt/ds3lab-scratch/ywattenberg/data/compact_CSJ_imgHD_subset_train.csv')
+    #test_data = pd.read_csv('/mnt/ds3lab-scratch/ywattenberg/data/compact_CSJ_imgHD_subset_test.csv')
+    num_users = df['userID'].nunique()
+    num_items = df['productID'].nunique()
+
+
+    model = torch.nn.DataParallel(ModelMatrixFactorization(num_items=num_items, num_users=num_users))
+    #model = torch.load('tmp_entire_model_imp.pth')
+    
+    train_data = AmazonCSJDataset(path=None, df=train_data)
+    test_data = AmazonCSJDataset(path=None, df=test_data)
+    loss_fn = torch.nn.MSELoss()
     optimizer = optimizer_fn(model.parameters(), lr=lr, weight_decay=weight_decay)
     
     return Trainer(model, train_data, test_data, loss_fn, optimizer, batch_size=batch_size, epochs=epochs)
